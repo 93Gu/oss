3f2892b0959721b2d78dbea9b1cdd8de6e9d8c12
93Gu <100657006+93Gu@users.noreply.github.com>
2025-06-25 09:15:53 +0800

    Update config.go

--- alioss/config.go
+++ alioss/config.go
@@ -6,4 +6,7 @@ type Config struct {
 	AccessKeySecret string
 	BucketName      string
 	BasePath        string // 可选：指定文件统一前缀路径
+	IsPrivate       bool   // true = use signed URL, false = public
+	EnableHashCheck bool   // true = 启用秒传（基于 md5）
+	MaxRetry        int    // 上传失败最大重试次数，默认可设为 3
 }
